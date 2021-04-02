e76e566555819f02ca456bd8bdc185e9ee5c354a
Folly135 <64118527+Folly135@users.noreply.github.com>
2021-04-02 13:08:17 +0800

    Fail try

--- model/features.py
+++ model/features.py
@@ -1,68 +1,106 @@
-#本模块用于提取音频信号的特征向量
+# 本模块用于提取音频信号的特征向量
 import sys
-
 import librosa as li
 import librosa.display as lid
-from python_speech_features import mfcc, delta, base
+# from python_speech_features import mfcc, delta, base
 
-#音频文件特征提取【MFCC】
-def MFCC(y,sr):
+###########################################################
+import scipy.io.wavfile
+import scipy
+from spafe.utils import vis
+from spafe.features.mfcc import mfcc
+from spafe.features.gfcc import gfcc
+
+
+# 音频文件特征提取【MFCC】
+def MFCC(y, sr):
     '''
     :param y:输入音频时间序列
     :param sr: 输入音频采样率
     :return: 输入音频的mfcc特征向量
     '''
 
-#1.librosa.feature.mfcc  【重要】
-#函数原型：librosa.feature.mfcc(y=None, sr=22050, S=None,
-# n_mfcc=20, dct_type=2, norm='ortho', **kwargs)
-#参数介绍：y：音频时间序列，np.ndarray [shape=(n,)] 或 None
-#         sr: y的采样率 number > 0 [scalar]
-#          S:对数功能梅尔谱图 np.ndarray [shape=(d, t)] or None
-#          n_mfcc:要返回的MFCC数量  int > 0 [scalar]
-#          dct_type:离散余弦变换（DCT）类型。默认情况下，使用DCT类型2 None, or {1, 2, 3}
-#          norm:规范。如果dct_type为2或3，则设置norm =’ortho’使用正交DCT基础。 None or ‘ortho’
-#           【 标准化不支持dct_type = 1。】
-#          kwargs:额外的关键参数 【参数melspectrogram，如果按时间序列输入操作】
-#          返回： MFCC序列 M:np.ndarray [shape=(n_mfcc, t)]  【？】
-    mfcc_feature=li.feature.mfcc(y=y,sr=sr,n_mfcc=39)
+    # 1.librosa.feature.mfcc  【重要】
+    # 函数原型：librosa.feature.mfcc(y=None, sr=22050, S=None, n_mfcc=20, dct_type=2, norm='ortho', **kwargs)
+    # 参数介绍：y：音频时间序列，np.ndarray [shape=(n,)] 或 None
+    #         sr: y的采样率 number > 0 [scalar]
+    #          S:对数功能梅尔谱图 np.ndarray [shape=(d, t)] or None
+    #          n_mfcc:要返回的MFCC数量  int > 0 [scalar]
+    #          dct_type:离散余弦变换（DCT）类型。默认情况下，使用DCT类型2 None, or {1, 2, 3}
+    #          norm:规范。如果dct_type为2或3，则设置norm =’ortho’使用正交DCT基础。 None or ‘ortho’
+    #           【 标准化不支持dct_type = 1。】
+    #          kwargs:额外的关键参数 【参数melspectrogram，如果按时间序列输入操作】
+    #          返回： MFCC序列 M:np.ndarray [shape=(n_mfcc, t)]  【？】
+    mfcc_feature = li.feature.mfcc(y=y, sr=sr, n_mfcc=39)
 
-#2.from python_speech_features import mfcc
-   # mfcc_feature = mfcc(wavedata, framerate, winlen=0.064, winstep=0.032, nfilt=13, nfft=1024)  # mfcc系数
+    # 2.from python_speech_features import mfcc
+    # mfcc_feature = mfcc(wavedata, framerate, winlen=0.064, winstep=0.032, nfilt=13, nfft=1024)  # mfcc系数
     # 其中wavedata为语音数据
     # framerate为采样率
     # winlen为帧长，单位为秒
     # winstep为帧移，单位为秒
     # nfilt为返回的mfcc数据维数，默认为13维（但经过我的实验，nfilt最多也只能返回13维的mfcc参数）
     # nfft为fft点数，一般要和帧长对应的采样点数要一样
-    #mfcc_feature=mfcc(y,sr)
-    #获取39维的MFCC特征
+    # mfcc_feature=mfcc(y,sr)
+    # 获取39维的MFCC特征
     # wav_feature = mfcc(y, sr)
     # d_mfcc_feat = delta(wav_feature, 1)
     # d_mfcc_feat2 = delta(wav_feature, 2)
     # mfcc_feature = np.hstack((wav_feature, d_mfcc_feat, d_mfcc_feat2))
 
-    #异常处理
-    if len(mfcc_feature)==0:
+    # 异常处理
+    if len(mfcc_feature) == 0:
         print >> sys.stderr, "ERROR.. failed to extract mfcc feature:", len(y)
 
-    return  mfcc_feature
+    return mfcc_feature
 
 
-
-#音频文件特征提取【GFCC】
-def GFCC(y,sr):
+# 音频文件特征提取【GFCC】
+def GFCC(fs, sig):
     '''
     提取输入文件的GFCC特征
     :param y:输入音频时间序列
     :param sr: 输入音频采样率
     :return: GFCC特征向量
     '''
-    pass
+    # spafe库能否实现gfcc？
+    # gfcc_feature = mfcc(sig, 13)
+    mfccs = mfcc(sig=sig,
+                 fs=fs,
+                 num_ceps=num_ceps,
+                 nfilts=nfilts,
+                 nfft=nfft,
+                 low_freq=low_freq,
+                 high_freq=high_freq,
+                 dct_type=dct_type,
+                 use_energy=use_energy,
+                 lifter=lifter,
+                 normalize=normalize)
+    # 参数列表：
+    # sig (array)：一种单声道音频信号（Nx1），用于计算特征
+    # fs (int)：正在处理信号的采样率 默认值为 16000_________
+    # num_ceps (float)：要返回的倒谱数 默认值为13_________
+    # pre_emph (int)：如果为1，则应用预强调。默认值为1_________
+    # pre_emph_coeff (float):应用预加重过滤器[1-预加重]（0=无）
+    # win_len (float):窗口长度（秒）。默认值为0.025_________
+    # win_hop (float):以秒为单位在连续窗口之间移动。默认值为0.01。_________
+    # win_type (float):窗口类型以应用窗口。默认值为“hamming”。_________
+    # nfilts (int):过滤器组中过滤器的数量。默认值为40。_________
+    # nfft (int):FFT点数。默认值为512。_________
+    # low_freq (int):mel滤波器的最低频带边缘（Hz）。默认值为0。_________
+    # high_freq (int):mel滤波器的最高频带边缘（Hz）。默认值为samplerate/2=8000。_________
+    # scale (str):选择最大bin振幅是上升、下降还是恒定（=1） 默认为恒定_________
+    # dct_type(int):使用的DCT类型-1或2（HTK为3，feac为4）。默认值为2。_________
+    # use_energy (int):用真对数能量覆盖C0默认值为0_________
+    # lifter (int):如果值>0，则应用提升,默认值为22_________
+    # normalize (int):如果为1,则应用规范化,默认值为0_________
+    if len(mfccs) == 0:
+        print >> sys.stderr, "ERROR.. failed to extract gfcc feature:", len(y)
+    return mfccs
 
 
-#音频文件特征提取【plp】
-def PLP(y,sr):
+# 音频文件特征提取【plp】
+def PLP(y, sr):
     '''
     提取输入音频文件的PLP特征
     :param y:输入音频时间序列
@@ -70,3 +108,34 @@ def PLP(y,sr):
     :return: PLP特征向量
     '''
     pass
+
+
+if __name__ == "__main__":
+    #################################
+    # init input vars
+    num_ceps = 13
+    low_freq = 0
+    high_freq = 2000
+    nfilts = 24
+    nfft = 512
+    dct_type = 2,
+    use_energy = False,
+    lifter = 5
+    normalize = False
+    #########################################
+    input = r'D:\Github_Desktop\BreathPrint\data\normal\hpc\正常呼吸2.wav'
+    fs, sig = scipy.io.wavfile.read(input)
+    y, sr = li.load(input)
+    print("librosa:")
+    print(y)
+    print("-------------------")
+    print(sr)
+    mfcc_li = MFCC(y, sr)
+    print(mfcc_li)
+    ##############################
+    print("spafe:")
+    print(fs)
+    print("-------------------")
+    print(sig)
+    gfccs  = gfcc(sig, 13)
+    vis.visualize(gfccs, 'LMFCC Coefficient Index', 'Frame Index')
--- model/gmm.py
+++ model/gmm.py
@@ -1,27 +1,27 @@
-#本模块构建GMM模型
+# 本模块构建GMM模型
 import pickle
 import time
 from collections import defaultdict
 
 import numpy as np
-from  sklearn.mixture import  GaussianMixture #导入高斯混合模型包
+from sklearn.mixture import GaussianMixture  # 导入高斯混合模型包
 import math
 import operator
 
 from model.features import MFCC
 
 
-class GMMSet:#GMM集合，每个GMM代表一个用户【或一种对象】
-    def __init__(self, gmm_order = 32):
+class GMMSet:  # GMM集合，每个GMM代表一个用户【或一种对象】
+    def __init__(self, gmm_order=32):
         '''
         初始化GMM模型【设置一些参数】
         :param gmm_order:GMM中高斯分布的数量，这里默认设置为13 [需要保证n_samples（MFCC维数） >= n_components]
         :param covariance_type: 协方差矩阵样式，默认设置为对角矩阵
         '''
-        self.gmms = [] #GMM列表，每个元素是一个高斯混合模型
-        self.gmm_order = gmm_order #模型中高斯分布的数量
-        #self.covariance_type=covariance_type  #模型协方差矩阵样式
-        self.y = [] #标签列表【一共有几类，每一类由用户名代表的标签表示】
+        self.gmms = []  # GMM列表，每个元素是一个高斯混合模型
+        self.gmm_order = gmm_order  # 模型中高斯分布的数量
+        # self.covariance_type=covariance_type  #模型协方差矩阵样式
+        self.y = []  # 标签列表【一共有几类，每一类由用户名代表的标签表示】
 
     def fit_new(self, x, label):
         '''
@@ -31,10 +31,10 @@ class GMMSet:#GMM集合，每个GMM代表一个用户【或一种对象】
         :return:gmms中新增一个GMM
         '''
         self.y.append(label)
-        #下面不知道为什么，协方差矩阵用对角矩阵diag型就不行
-        gmm = GaussianMixture(self.gmm_order,covariance_type='diag') #初始化一个高斯混合模型类，这里设置高斯分布数量为32，协方差矩阵为对角矩阵
-        gmm.fit(x) #根据输入的特征向量，使用EM算法估算模型参数,返回训练后的模型
-        self.gmms.append(gmm) #将得到的高斯混合模型加入模型集合
+        # 下面不知道为什么，协方差矩阵用对角矩阵diag型就不行
+        gmm = GaussianMixture(self.gmm_order, covariance_type='diag')  # 初始化一个高斯混合模型类，这里设置高斯分布数量为32，协方差矩阵为对角矩阵
+        gmm.fit(x)  # 根据输入的特征向量，使用EM算法估算模型参数,返回训练后的模型
+        self.gmms.append(gmm)  # 将得到的高斯混合模型加入模型集合
 
     def gmm_score(self, gmm, x):
         '''
@@ -43,9 +43,9 @@ class GMMSet:#GMM集合，每个GMM代表一个用户【或一种对象】
         :param x: 用于测试的特征向量矩阵
         :return: 计算得分【把x中每一个特征向量的得分加起来作为最后的得分】
         '''
-        return np.sum(gmm.score(x))  #gmm.scroe返回一个列表，列表中每个元素是X的一个特征向量的得分，np.sum()计算列表元素的和，作位最终结果返回
+        return np.sum(gmm.score(x))  # gmm.scroe返回一个列表，列表中每个元素是X的一个特征向量的得分，np.sum()计算列表元素的和，作位最终结果返回
 
-    @staticmethod #静态方法，只是名义上归属类管理，但是不能使用类变量和实例变量，是类的工具包
+    @staticmethod  # 静态方法，只是名义上归属类管理，但是不能使用类变量和实例变量，是类的工具包
     def softmax(scores):
         '''
         分类器，使用softmax函数进行计算每个类别的“概率”【softmax的作用就是将得分映射到0-1之前，类似于概率的形式】
@@ -53,8 +53,8 @@ class GMMSet:#GMM集合，每个GMM代表一个用户【或一种对象】
         :return: 最大概率占总体的比重
         '''
         scores_sum = sum([math.exp(i) for i in scores])
-        score_max  = math.exp(max(scores)) #取出最大得分作为e的指数
-        return round(score_max / (scores_sum), 3) #round() 方法返回浮点数x的四舍五入值【3表示保留小数点后三位】
+        score_max = math.exp(max(scores))  # 取出最大得分作为e的指数
+        return round(score_max / (scores_sum), 3)  # round() 方法返回浮点数x的四舍五入值【3表示保留小数点后三位】
 
     def predict_one(self, x):
         '''
@@ -62,19 +62,20 @@ class GMMSet:#GMM集合，每个GMM代表一个用户【或一种对象】
         :param x:输入的音频特征向量
         :return: 返回类别（用户名/姓名。。）和评分
         '''
-        scores = [self.gmm_score(gmm, x) / len(x) for gmm in self.gmms]#获取每个人gmm评分的【均值】
-        #p = sorted(enumerate(scores), key=operator.itemgetter(1), reverse=True)#根据gmm评分排序
-        #p = [(str(self.y[i]), y, p[0][1] - y) for i, y in p]
-        result = [(self.y[index], value) for (index, value) in enumerate(scores)]#用标签取代scores中的index，每个标签代表一个用户，用户有其自己的GMM模型
-        p = max(result, key=operator.itemgetter(1)) #获取评分最大值
+        scores = [self.gmm_score(gmm, x) / len(x) for gmm in self.gmms]  # 获取每个人gmm评分的【均值】
+        # p = sorted(enumerate(scores), key=operator.itemgetter(1), reverse=True)#根据gmm评分排序
+        # p = [(str(self.y[i]), y, p[0][1] - y) for i, y in p]
+        result = [(self.y[index], value) for (index, value) in
+                  enumerate(scores)]  # 用标签取代scores中的index，每个标签代表一个用户，用户有其自己的GMM模型
+        p = max(result, key=operator.itemgetter(1))  # 获取评分最大值
         # 【operator.itemgetter()：返回一个可调用对象，用于从运算对象中获取元素】，括号里面的1表示选择result中的第二项，即分数项
-        softmax_score = self.softmax(scores)#映射为0-1之间
-        return p[0], softmax_score#返回姓名和评分
+        softmax_score = self.softmax(scores)  # 映射为0-1之间
+        return p[0], softmax_score  # 返回姓名和评分
 
-    def before_pickle(self): #使用pickle保存模型之前
+    def before_pickle(self):  # 使用pickle保存模型之前
         pass
 
-    def after_pickle(self): #使用pickle保存模型之后
+    def after_pickle(self):  # 使用pickle保存模型之后
         pass
 
 
@@ -87,15 +88,15 @@ class GMMModel:
         self.features = defaultdict(list)  # 当字典的key不存在时，返回的是[],即一个空的列表
         self.gmmset = GMMSet()  # 获取GMM模型组
 
-    def enroll(self,name,y,sr):
+    def enroll(self, name, y, sr):
         '''
         对输入音频进行特征提取，而后以键值对的方式加入features字典中
         :param name:标签【用户名/姓名】
         :param y:音频时间序列
         :param sr:采样率
         '''
-        feat=np.array(MFCC(y,sr)) #提取MFCC特征,并转换为np数组
-        self.features[name].extend(feat) #加入字典
+        feat = np.array(MFCC(y, sr))  # 提取MFCC特征,并转换为np数组
+        self.features[name].extend(feat)  # 加入字典
 
     def train(self):
         '''
@@ -107,10 +108,10 @@ class GMMModel:
             try:
                 self.gmmset.fit_new(np.array(feats), name)  # 对每一个用户，训练出一个GMM模型后，加入到GMM模型组中
             except Exception as e:
-                print("%s failed %s" % (name,e))  # 打印处理那个用户时发生了错误
+                print("%s failed %s" % (name, e))  # 打印处理那个用户时发生了错误
         print(time.time() - start_time, " seconds")  # 打印训练所需的时间
 
-    def predict(self,y,sr):
+    def predict(self, y, sr):
         '''
         进行预测
         :param y:输入音频的时间序列
@@ -118,21 +119,20 @@ class GMMModel:
         :return: 预测的标签【用户名/姓名】、评分
         '''
         try:
-            feat=MFCC(y,sr)
+            feat = MFCC(y, sr)
         except Exception as e:
             print(e)
-        return  self.gmmset.predict_one(feat)
+        return self.gmmset.predict_one(feat)
 
-    def save(self,fname):
+    def save(self, fname):
         '''
         保存训练好的模型
         :param fanme: 保存模型的文件名
         '''
-        #使用pickle模块
-        with open(fname,'wb') as f: #写入模型
-            pickle.dump(self,f,-1)
-        #使用
-
+        # 使用pickle模块
+        with open(fname, 'wb') as f:  # 写入模型
+            pickle.dump(self, f, -1)
+        # 使用
 
     def load(fname):
         '''
@@ -140,6 +140,6 @@ class GMMModel:
         fname:文件名
         :return:返回模型
         '''
-        with open(fname,'rb') as f: #以只读方式打开文件
-            M=pickle.load(f) #加载之前保存的模型
+        with open(fname, 'rb') as f:  # 以只读方式打开文件
+            M = pickle.load(f)  # 加载之前保存的模型
             return M
